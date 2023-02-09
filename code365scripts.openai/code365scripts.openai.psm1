0803cf785ffe881cf5f14f2bf392153748245193
chenxizhang <ares@xizhang.com>
2023-02-09 22:44:30 +0800

    update openai module

--- code365scripts.openai/code365scripts.openai.psm1
+++ code365scripts.openai/code365scripts.openai.psm1
@@ -4,25 +4,29 @@
     param(
         [Parameter()][string]$api_key = $env:OPENAI_API_KEY,
         [Parameter()][string]$engine = "text-davinci-003",
-        [Parameter()][string]$max_tokens = 1024
+        [Parameter()][int]$max_tokens = 1024
     )
 
-    Write-Host "欢迎来到OpenAI的世界，请输入你的提示，按 q 退出.`n"
+    Write-Host "欢迎来到OpenAI的世界，请输入你的提示，按 q 退出."
 
     while ($true) {
-        $prompt = Read-Host -Prompt "提示"
+        $prompt = Read-Host -Prompt "`n提示"
 
         if ($prompt -eq "q") {
             break
         }
 
-        $body = @{
-            model      = $engine
-            prompt     = $prompt
-            max_tokens = $max_tokens
-        } | ConvertTo-Json
+        $params = @{
+            Uri         = "https://api.openai.com/v1/completions"
+            Method      = "POST"
+            Body        = @{model = "$engine"; prompt = "$prompt"; max_tokens = $max_tokens } | ConvertTo-Json
+            Headers     = @{"Authorization" = "Bearer $api_key" }
+            ContentType = "application/json;charset=utf-8"
+        }
         
-        Invoke-WebRequest -Uri "https://api.openai.com/v1/completions" -Headers @{"Content-Type" = "application/json"; "Authorization" = "Bearer $api_key" } -Body $body
+        $response = (Invoke-WebRequest @params).Content | ConvertFrom-Json
+
+        Write-Host $response.choices[0].text -ForegroundColor Green
 
     }
 }
