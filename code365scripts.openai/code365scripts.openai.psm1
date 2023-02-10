2a9c967f5ae7e8aa63513844bad81fa3b387b7dd
Ares Chen <arechen@microsoft.com>
2023-02-10 17:38:22 +0800

    输入支持多行 Fixes #3

--- code365scripts.openai/code365scripts.openai.psm1
+++ code365scripts.openai/code365scripts.openai.psm1
@@ -4,10 +4,11 @@
     param(
         [Parameter()][string]$api_key = $env:OPENAI_API_KEY,
         [Parameter()][string]$engine = "text-davinci-003",
-        [Parameter()][int]$max_tokens = 1024
+        [Parameter()][int]$max_tokens = 1024,
+        [Parameter()][double]$temperature = 1
     )
 
-    Write-Host "欢迎来到OpenAI的世界，请输入你的提示，按 q 退出."
+    Write-Host "欢迎来到OpenAI的世界，请输入你的提示，按 q 退出, 按 m 输入多行文本."
 
     while ($true) {
         $prompt = Read-Host -Prompt "`n提示"
@@ -16,17 +17,142 @@
             break
         }
 
+        if ($prompt -eq "m") {
+            # 这是用户想要输入多行文本
+            $prompt = Read-MultiLineInputBoxDialog -Message "请输入多行文本" -WindowTitle "多行文本" -DefaultText ""
+            if ($null -eq $prompt) {
+                Write-Host "你按下了取消按钮"
+                continue
+            }
+            else {
+                Write-Host "你输入的多行文本是：`n$prompt"
+            }
+        }
+
         $params = @{
             Uri         = "https://api.openai.com/v1/completions"
             Method      = "POST"
-            Body        = @{model = "$engine"; prompt = "$prompt"; max_tokens = $max_tokens } | ConvertTo-Json
+            Body        = @{model = "$engine"; prompt = "$prompt"; max_tokens = $max_tokens; temperature = $temperature } | ConvertTo-Json
             Headers     = @{"Authorization" = "Bearer $api_key" }
             ContentType = "application/json;charset=utf-8"
         }
         
         $response = (Invoke-WebRequest @params).Content | ConvertFrom-Json
 
+        Write-Host -ForegroundColor Red ("`n回答: - 消耗的token数量: {0} = {1} + {2}" -f $response.usage.total_tokens, $response.usage.prompt_tokens, $response.usage.completion_tokens )
         Write-Host $response.choices[0].text -ForegroundColor Green
 
     }
+}
+
+
+function Read-MultiLineInputBoxDialog([string]$Message, [string]$WindowTitle, [string]$DefaultText) {
+    <#
+    .SYNOPSIS
+    Prompts the user with a multi-line input box and returns the text they enter, or null if they cancelled the prompt.
+
+    .DESCRIPTION
+    Prompts the user with a multi-line input box and returns the text they enter, or null if they cancelled the prompt.
+
+    .PARAMETER Message
+    The message to display to the user explaining what text we are asking them to enter.
+
+    .PARAMETER WindowTitle
+    The text to display on the prompt window's title.
+
+    .PARAMETER DefaultText
+    The default text to show in the input box.
+
+    .EXAMPLE
+    $userText = Read-MultiLineInputDialog "Input some text please:" "Get User's Input"
+
+    Shows how to create a simple prompt to get mutli-line input from a user.
+
+    .EXAMPLE
+    # Setup the default multi-line address to fill the input box with.
+    $defaultAddress = @'
+    John Doe
+    123 St.
+    Some Town, SK, Canada
+    A1B 2C3
+    '@
+
+    $address = Read-MultiLineInputDialog "Please enter your full address, including name, street, city, and postal code:" "Get User's Address" $defaultAddress
+    if ($address -eq $null)
+    {
+        Write-Error "You pressed the Cancel button on the multi-line input box."
+    }
+
+    Prompts the user for their address and stores it in a variable, pre-filling the input box with a default multi-line address.
+    If the user pressed the Cancel button an error is written to the console.
+
+    .EXAMPLE
+    $inputText = Read-MultiLineInputDialog -Message "If you have a really long message you can break it apart`nover two lines with the powershell newline character:" -WindowTitle "Window Title" -DefaultText "Default text for the input box."
+
+    Shows how to break the second parameter (Message) up onto two lines using the powershell newline character (`n).
+    If you break the message up into more than two lines the extra lines will be hidden behind or show ontop of the TextBox.
+
+    .NOTES
+    Name: Show-MultiLineInputDialog
+    Author: Daniel Schroeder (originally based on the code shown at http://technet.microsoft.com/en-us/library/ff730941.aspx)
+    Version: 1.0
+#>
+    Add-Type -AssemblyName System.Drawing
+    Add-Type -AssemblyName System.Windows.Forms
+
+    # Create the Label.
+    $label = New-Object System.Windows.Forms.Label
+    $label.Location = New-Object System.Drawing.Size(10, 10)
+    $label.Size = New-Object System.Drawing.Size(280, 20)
+    $label.AutoSize = $true
+    $label.Text = $Message
+
+    # Create the TextBox used to capture the user's text.
+    $textBox = New-Object System.Windows.Forms.TextBox
+    $textBox.Location = New-Object System.Drawing.Size(10, 40)
+    $textBox.Size = New-Object System.Drawing.Size(575, 200)
+    $textBox.AcceptsReturn = $true
+    $textBox.AcceptsTab = $false
+    $textBox.Multiline = $true
+    $textBox.ScrollBars = 'Both'
+    $textBox.Text = $DefaultText
+
+    # Create the OK button.
+    $okButton = New-Object System.Windows.Forms.Button
+    $okButton.Location = New-Object System.Drawing.Size(415, 250)
+    $okButton.Size = New-Object System.Drawing.Size(75, 25)
+    $okButton.Text = "确定"
+    $okButton.Add_Click({ $form.Tag = $textBox.Text; $form.Close() })
+
+    # Create the Cancel button.
+    $cancelButton = New-Object System.Windows.Forms.Button
+    $cancelButton.Location = New-Object System.Drawing.Size(510, 250)
+    $cancelButton.Size = New-Object System.Drawing.Size(75, 25)
+    $cancelButton.Text = "取消"
+    $cancelButton.Add_Click({ $form.Tag = $null; $form.Close() })
+
+    # Create the form.
+    $form = New-Object System.Windows.Forms.Form
+    $form.Text = $WindowTitle
+    $form.Size = New-Object System.Drawing.Size(610, 320)
+    $form.FormBorderStyle = 'FixedSingle'
+    $form.StartPosition = "CenterScreen"
+    $form.AutoSizeMode = 'GrowAndShrink'
+    $form.Topmost = $True
+    $form.AcceptButton = $okButton
+    $form.CancelButton = $cancelButton
+    $form.ShowInTaskbar = $true
+
+    # Add all of the controls to the form.
+    $form.Controls.Add($label)
+    $form.Controls.Add($textBox)
+    $form.Controls.Add($okButton)
+    $form.Controls.Add($cancelButton)
+
+    # Initialize and show the form.
+    $form.Add_Shown({ $form.Activate() })
+    $form.ShowDialog() > $null  # Trash the text of the button that was clicked.
+
+    # Return the text that the user entered.
+    return $form.Tag
 }
